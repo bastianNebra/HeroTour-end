929f76d489cf8b72d4dbdb810bdba9d2bacb67c5
blontsi <123bastianlontsi@gmail.com>
2022-11-08 10:18:11 +0100

    Edit Hero Component

---
+++ HeroTour_rgnx/src/app/edit-hero/edit-hero.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-edit-hero',
+  templateUrl: './edit-hero.component.html',
+  styleUrls: ['./edit-hero.component.css']
+})
+export class EditHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
